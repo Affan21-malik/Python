76a859deafc19d6730af1f1e785cc8a7e9cb5a44
mohd Affan <mohdaffanboy786@gmail.com>
2026-08-29 10:57:46 +0530

    set are finished

---
+++ ch4/3.py
@@ -0,0 +1,42 @@
+# set in python  set ma each element must be unique and ye immutable h ok 
+# agr element unique nhi h to bo egonre kreg arepeted value ko bus ek baar print kraga 
+"""collection ={1,2,3,4,5}
+print(collection)
+print(type(collection))
+print(len(collection)) 
+# ye len egnore krne k baad bbtayega ki kitni value h ok
+
+"""
+
+ #collection={} eska type dic hoga 
+ # collection=set() eska type set hoga 
+"""
+
+collection = set()
+collection.add(1)
+collection.add(1)
+collection.add(2)
+collection.remove(1)
+collection.add((1,2,3))m y eshi h but 
+collection.add([1,2,3]) ye error dega ok 
+
+# [] ye list h or ye mutuble h esliye ye set m add nhi hogi ok
+print(collection)
+
+# collection.clear() essa sb kuch hat jayega 
+#collection.pop() ye random value ko hta deta h 
+
+"""
+
+"""
+
+set1={1,2,3}
+set2={2,3,4}
+print(set1.union(set2))
+print(set1.intersection(set2))
+"""
+
+
+
+
+
